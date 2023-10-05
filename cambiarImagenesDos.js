382562f5a86f4baeead4b3413eae7360eb9b351b
Nuev3Caos <105306610+Nuev3Caos@users.noreply.github.com>
2023-10-05 16:47:05 +0200

    borrado

--- cambiarImagenesDos.js
+++ cambiarImagenesDos.js
@@ -1,44 +1,4 @@
-//imagenes
-
- 
-
-    var imagenes=new Array(
-
-        ['img/una.png'],
-
-        ['img/dos.png'],
-
-    );
-
-    var contador=0;
-
-
-    function rotarImagenes()
-
-    {
-     
-      contador++
-
-      document.getElementById("mages").src=imagenes[contador%imagenes.length][0];
-      
-
-    }
-
-
-    onload=function()
-
-    {
-
-        rotarImagenes();
-
-        setInterval(rotarImagenes,3000);
-
-    }
-
-
-
- 
-
+//i
 
  
 
