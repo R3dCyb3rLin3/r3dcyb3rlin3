f97973278de1e4b2e5a82c8b26d806e1b33394a3
RedCyb3r <105306610+RedCyb3r@users.noreply.github.com>
2024-12-23 15:35:23 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-// JavaScript para el menú desplegable en dispositivos móviles
-const menuToggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
-
-menuToggle.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
+function toggleMenu() {
+  const menu = document.getElementById('menu');
+  menu.classList.toggle('show');
+}
