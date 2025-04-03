54cd7d9a13d87db5add5e02d72edc9770a7c7a0e
RedCyb3r <105306610+RedCyb3r@users.noreply.github.com>
2025-04-03 16:27:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,4 @@
-// Verificar si el navegador soporta la API de Bluetooth
-if (!navigator.bluetooth) {
-    alert("Tu navegador no soporta la API Web Bluetooth.");
-}
-
-document.getElementById("buscar").addEventListener("click", function() {
-    // Iniciar la búsqueda de dispositivos Bluetooth
-    navigator.bluetooth.requestDevice({
-        acceptAllDevices: true,
-        optionalServices: ['battery_service'] // Puedes agregar otros servicios Bluetooth aquí
-    })
-    .then(device => {
-        // Mostrar el nombre del dispositivo encontrado
-        console.log("Dispositivo encontrado:", device.name);
-        addDeviceToList(device.name);
-    })
-    .catch(error => {
-        console.log("Error al encontrar el dispositivo:", error);
-    });
-});
-
-// Función para añadir un dispositivo a la lista en la interfaz de usuario
-function addDeviceToList(deviceName) {
-    const list = document.getElementById("listaDispositivos");
-    const listItem = document.createElement("li");
-    listItem.textContent = deviceName;
-    list.appendChild(listItem);
+function toggleMenu() {
+  const menu = document.getElementById('menu');
+  menu.classList.toggle('show');
 }
